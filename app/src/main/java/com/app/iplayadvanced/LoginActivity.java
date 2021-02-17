81fd2b339224dcde152db0bed9cc38ad180efe5a
mouaadblhn <mouaadblhn@gmail.com>
2021-02-17 02:30:25 +0100

    Add HomeActivity & LoginActivity

--- app/src/main/java/com/app/iplayadvanced/LoginActivity.java
+++ app/src/main/java/com/app/iplayadvanced/LoginActivity.java
@@ -2,7 +2,9 @@ package com.app.iplayadvanced;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view) {
+        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
     }
 }
